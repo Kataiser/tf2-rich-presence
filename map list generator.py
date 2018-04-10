1a6e0d416f27f214b72b3be8a01d099b5bcf1000
Kataiser <mecharon1.gm@gmail.com>
2018-04-10 12:53:28 -0500

    Map building gets updated map list
    Also updated the venv

--- map list generator.py
+++ map list generator.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 gamemodes = {'Capture the Flag': 'ctf', 'Control Point': 'control-point', 'Attack/Defend': 'attack-defend', 'Attack/Defend (Medieval Mode)': 'medieval-mode',
              'Control Point (Domination)': 'control-point', 'Territorial Control': 'territorial-control', 'Payload': 'payload', 'Payload Race': 'payload-race',
@@ -6,8 +7,8 @@ gamemodes = {'Capture the Flag': 'ctf', 'Control Point': 'control-point', 'Attac
              'PASS Time': 'passtime', 'Player Destruction': 'player-destruction', 'Attack/Defend(Medieval Mode)': 'attack-defend', 'Control Point(Domination)': 'control-point'}
 map_gamemodes = {}
 
-with open('List of maps - Official TF2 Wiki _ Official Team Fortress Wiki.html') as fp:
-    soup = BeautifulSoup(fp, 'lxml')
+r = requests.get('https://wiki.teamfortress.com/wiki/List_of_maps')
+soup = BeautifulSoup(r.text, 'lxml')
 
 for tr in soup.find_all('tr'):
     map_file, map_name, map_mode = (None, None, None)
