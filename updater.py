49314f62765b1d59db372dba4b3b856bc1c78241
Kataiser <mecharon1.gm@gmail.com>
2018-07-23 18:25:23 -0500

    Fixed a crash when checking for updates

--- updater.py
+++ updater.py
@@ -1,3 +1,5 @@
+import traceback
+
 import requests
 
 import logger as log
@@ -5,14 +7,15 @@ import logger as log
 
 def check(current_version, timeout):
     log.debug(f"Checking for updates, timeout: {timeout} secs")
+
     try:
         r = requests.get('https://api.github.com/repos/Kataiser/tf2-rich-presence/releases/latest', timeout=timeout)
     except requests.exceptions.Timeout:
         log.error(f"Update check timed out")
-        line1 = "Couldn't connect to GitHub to check for updates (timed out after {} seconds).\n".format(timeout)
-        line2 = "To check for updates yourself, go to https://github.com/Kataiser/tf2-rich-presence/releases\n"
-        line3 = "(you are currently running {}).\n".format(current_version)
-        print(f"{line1}{line2}{line3}")
+        failure_message(current_version, f"(timed out after {timeout} seconds)")
+    except Exception as other_error:
+        log.error(f"Non-timout update error: {traceback.format_exc()}")
+        failure_message(failure_message(current_version, other_error))
     else:
         response = r.json()
         newest_version = response['tag_name']
@@ -21,6 +24,13 @@ def check(current_version, timeout):
             log.error(f"Out of date, newest version is {newest_version}")
             downloads_url = response['html_url']
             print(f"This version ({current_version}) is out of date (newest version is {newest_version}).\nGet the update at {downloads_url}\n")
+
+
+def failure_message(current_version, error_message):
+    line1 = f"Couldn't connect to GitHub to check for updates {error_message}.\n"
+    line2 = "To check for updates yourself, go to https://github.com/Kataiser/tf2-rich-presence/releases\n"
+    line3 = f"(you are currently running {current_version}).\n"
+    print(f"{line1}{line2}{line3}")
 
 
 if __name__ == '__main__':
