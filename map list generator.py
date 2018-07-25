0444c56549921343a11babb0f89985e35838d250
Kataiser <mecharon1.gm@gmail.com>
2018-07-24 21:29:21 -0500

    Changed cp_cloak's gamemode

--- map list generator.py
+++ map list generator.py
@@ -7,7 +7,7 @@ gamemodes = {'Capture the Flag': 'ctf', 'Control Point': 'control-point', 'Attac
              'Control Point (Domination)': 'control-point', 'Territorial Control': 'territorial-control', 'Payload': 'payload', 'Payload Race': 'payload-race',
              'King of the Hill': 'koth', 'Special Delivery': 'special-delivery', 'Mann vs. Machine': 'mvm', 'Robot Destruction': 'beta-map', 'Mannpower': 'mannpower',
              'PASS Time': 'passtime', 'Player Destruction': 'player-destruction', 'Attack/Defend(Medieval Mode)': 'attack-defend', 'Control Point(Domination)': 'control-point',
-             'Arena': 'arena', 'Training Mode': 'training', 'Developer aidTest': 'unknown_map', 'Developer aidControl Point': 'unknown_map'}
+             'Arena': 'arena', 'Training Mode': 'training', 'Developer aidTest': 'unknown_map', 'Developer aidControl Point': 'control-point'}
 map_gamemodes = {}
 
 r = requests.get('https://wiki.teamfortress.com/wiki/List_of_maps')
@@ -30,9 +30,8 @@ for tr in soup.find_all('tr'):
 
     if map_mode and map_name:
         map_file = map_file.replace(' ', '')
-        gamemode_replacements = [('d(M', 'd (M'), ('t(D', 't (D'), (' Mode', ''), ('Developer aidTest', 'No gamemode'), ('Developer aidControl Point', 'No gamemode')]
+        gamemode_replacements = [('d(M', 'd (M'), ('t(D', 't (D'), (' Mode', ''), ('Developer aidTest', 'No gamemode'), ('Developer aidControl Point', 'Control Point')]
         for replacemment in gamemode_replacements:
-            print(gamemode_fancy, replacemment)
             gamemode_fancy = gamemode_fancy.replace(*replacemment)
 
         map_gamemodes[map_file] = (map_name, map_mode, gamemode_fancy)
