de4c6f13900660109566a9ad20f8b799b73c94c0
Kataiser <mecharon1.gm@gmail.com>
2018-08-07 22:19:39 -0500

    Launching no longer uses a browser

--- TF2 Rich Presence/Launch TF2 with Rich Presence.bat
+++ TF2 Rich Presence/Launch TF2 with Rich Presence.bat
@@ -2,10 +2,10 @@
 echo TF2 Rich Presence ({tf2rpvnum}) by Kataiser
 echo https://github.com/Kataiser/tf2-rich-presence
 echo.
-echo Launching Team Fortress 2, with Rich Presence enabled, via your default browser...
+echo Launching Team Fortress 2 with Rich Presence enabled...
 echo.
 
-start "" steam://rungameid/440
+start steam://rungameid/440
 "%~dp0\resources\python\python.exe" "%~dp0\resources\updater.py"
 
 :start
