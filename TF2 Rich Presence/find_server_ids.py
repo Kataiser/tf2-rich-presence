60eda7de497af61c9865ef9efdddb97d38d3f0b8
Kataiser <mecharon1.gm@gmail.com>
2018-09-09 17:31:56 -0500

    Improved and updated server providers data and generation

--- TF2 Rich Presence/find_server_ids.py
+++ TF2 Rich Presence/find_server_ids.py
@@ -5,7 +5,11 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    out = {}
+    try:
+        with open('community_server_ips.json', 'r') as community_server_ips_json:
+            out = json.load(community_server_ips_json)
+    except FileNotFoundError:
+        out = {}
 
     providers_page = requests.get('https://teamwork.tf/community/providers').text
     providers_page_soup = BeautifulSoup(providers_page, 'lxml')
@@ -19,7 +23,11 @@ def main():
                     provider_name = inner_div.find('h3').string
 
             print(provider_name, provider_page_url)
-            ips = set()
+
+            try:
+                ips: list = out[provider_name]
+            except KeyError:
+                ips = []
 
             provider_page = requests.get(provider_page_url).text
             provider_page_soup = BeautifulSoup(provider_page, 'lxml')
@@ -27,12 +35,14 @@ def main():
             for kbd in provider_page_soup.find_all('kbd'):
                 kbd_string = str(kbd.string)
 
-                if ':' in kbd_string:
-                    ips.add(kbd_string)
+                if ':' in kbd_string and kbd_string not in ips:
+                    ips.append(kbd_string)
 
-            ips_list = list(ips)
-            print(ips_list)
-            out[provider_name] = ips_list
+            print(ips)
+            out[provider_name] = ips
+
+            if not ips:
+                del out[provider_name]
 
     with open('community_server_ips.json', 'w') as community_server_ips_json:
         json.dump(out, community_server_ips_json, indent=4)
