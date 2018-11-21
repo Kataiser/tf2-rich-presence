45631248571803baef3eefa0e73c94449b1ef33a
Kataiser <mecharon1.gm@gmail.com>
2018-11-20 22:05:25 -0600

    Minor improvements to find_server_ids

--- TF2 Rich Presence/find_server_ids.py
+++ TF2 Rich Presence/find_server_ids.py
@@ -35,7 +35,8 @@ def main():
             provider_page = get_with_retries(provider_page_url).text
             provider_page_soup = BeautifulSoup(provider_page, 'lxml')
 
-            ips = []  # only used for progress display
+            ip_pairs = []  # only used for progress display
+            ips = []  # ditto
             server_name = ''
 
             # every server a provider... provides
@@ -45,14 +46,18 @@ def main():
                         server_name: str = div_in_server_div.string
                         server_name = server_name.lstrip().rstrip()  # remove leading and trailing whitespace
 
+                        if 'TF2Maps.net | Frankfurt' in server_name:
+                            pass
+
                 for kbd in server_div.find_all('kbd'):  # kbd tag = keyboard. means monospace font in HTML
                     kbd_string = str(kbd.string)
 
-                    if ':' in kbd_string:  # kbd tags are used for several things. this makes sure it's an IP
-                        ips.append((kbd_string, server_name))
+                    if ':' in kbd_string and kbd_string not in ips:  # kbd tags are used for several things. this makes sure it's an IP
+                        ip_pairs.append((kbd_string, server_name))
+                        ips.append(kbd_string)
                         out[kbd_string] = (provider_name, server_name)
 
-            print(ips)
+            print(ip_pairs)
 
     with open(save_file_name, 'wb') as community_server_ips_json:
         community_server_ips_json.write(json.dumps(out, indent=4, ensure_ascii=False).encode('utf8'))  # the encoding stuff is because the json library hates unicode
@@ -60,11 +65,17 @@ def main():
 
 # a requests.get with error handling in an infinite loop
 def get_with_retries(url):
+    retries_made = 0
+
     while True:
         try:
-            return requests.get(url, timeout=15)
+            return requests.get(url, timeout=2)
         except Exception as error:
             print(f"Error while accessing URL {url}: ({error}). Retrying...", file=sys.stderr)
+
+            retries_made += 1
+            if retries_made == 10 and 'providers' not in url:
+                pass
 
 
 if __name__ == '__main__':
