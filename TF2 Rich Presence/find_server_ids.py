0d10a9807d52a396ebb8d7829765cee8e7b573c5
Kataiser <mecharon1.gm@gmail.com>
2018-12-23 21:30:59 -0600

    Improved server database generator

--- TF2 Rich Presence/find_server_ids.py
+++ TF2 Rich Presence/find_server_ids.py
@@ -22,7 +22,7 @@ def main():
         out = {}
 
     # download the main providers page
-    providers_page = get_with_retries('https://teamwork.tf/community/providers').text
+    providers_page = get_with_retries('https://teamwork.tf/community/providers')
     providers_page_soup = BeautifulSoup(providers_page, 'lxml')
 
     for outer_div in providers_page_soup.find_all('div'):  # trying to find a link to each provider page
@@ -37,7 +37,7 @@ def main():
             print(provider_name, provider_page_url)
 
             # download the particular provider page
-            provider_page = get_with_retries(provider_page_url).text
+            provider_page = get_with_retries(provider_page_url)
             provider_page_soup = BeautifulSoup(provider_page, 'lxml')
 
             ip_pairs = []  # only used for progress display
@@ -57,7 +57,7 @@ def main():
                     if ':' in kbd_string and kbd_string not in ips:  # kbd tags are used for several things. this makes sure it's an IP
                         ip_pairs.append((kbd_string, server_name))
                         ips.append(kbd_string)
-                        out[kbd_string] = (provider_name, server_name)
+                        out[kbd_string] = [provider_name, server_name]
 
             print(ip_pairs)
 
@@ -74,7 +74,7 @@ def main():
                     break
 
     for ip_to_delete in ips_to_delete:
-        del out[ip_to_delete]
+        out[ip_to_delete][1] = "Unknown server name"
     print(f"\nRemoved {len(ips_to_delete)} servers with map names")
 
     with open(save_file_name, 'wb') as community_server_ips_json:
@@ -84,16 +84,28 @@ def main():
 # a requests.get with error handling in an infinite loop
 def get_with_retries(url):
     retries_made = 0
+    is_providers_page = 'providers' in url
 
     while True:
         try:
-            return requests.get(url, timeout=2)
+            if is_providers_page:
+                timeout_ = 10
+            else:
+                timeout_ = 2
+
+            response = requests.get(url, timeout=timeout_).text
+
+            if isinstance(response, str):
+                return response
+            else:
+                raise ConnectionError("Response text is None")
         except Exception as error:
             print(f"Error while accessing URL {url}: ({error}). Retrying...", file=sys.stderr)
 
             retries_made += 1
-            if retries_made == 10 and 'providers' not in url:
-                pass
+            if retries_made == 10 and not is_providers_page:
+                print(f"Skipping {url}", file=sys.stderr)
+                break
 
 
 if __name__ == '__main__':
