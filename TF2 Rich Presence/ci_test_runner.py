1b034fe58dfa0142ad077890f86cd630cad0ae6e
Kataiser <mecharon1.gm@gmail.com>
2019-06-05 14:10:35 -0500

    More CI fixes

--- TF2 Rich Presence/ci_test_runner.py
+++ TF2 Rich Presence/ci_test_runner.py
@@ -1,11 +1,13 @@
 import tests
+import os
 
 # for https://travis-ci.org/Kataiser/tf2-rich-presence
 if __name__ == '__main__':
+    print(os.listdir(os.getcwd()))
+
     f_tests = tests.TestTF2RichPresenseFunctions
     ci_tests = [f_tests.test_interpret_console_log, f_tests.test_steam_config_file, f_tests.test_logger, f_tests.test_log_cleanup, f_tests.test_generate_hash, f_tests.test_read_truncated_file,
-                f_tests.test_pastebin, f_tests.test_access_github_api, f_tests.test_settings_check_int, f_tests.test_settings_access, f_tests.test_get_api_key, f_tests.test_compact_file,
-                f_tests.test_generate_delta, f_tests.test_get_info_from_pid]
+                f_tests.test_settings_check_int, f_tests.test_settings_access, f_tests.test_get_api_key, f_tests.test_compact_file, f_tests.test_generate_delta, f_tests.test_get_info_from_pid]
 
     for ci_test in ci_tests:
         # this is probably broken in some way
