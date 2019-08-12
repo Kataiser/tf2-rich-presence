a02caeae5739a6368dd8fb4d336e764fcd8b281b
Kataiser <mecharon1.gm@gmail.com>
2019-08-12 12:09:20 -0500

    Updated build version number to v1.9

--- TF2 Rich Presence/build_version.py
+++ TF2 Rich Presence/build_version.py
@@ -1,2 +1,2 @@
-version_hashes = {'v1.7.4': 'a56ec211', 'v1.8': '3f041c52', 'v1.8.1': 'b4db39db'}
-this_version = 'v1.8.1'
+version_hashes = {'v1.7.4': 'a56ec211', 'v1.8': '3f041c52', 'v1.8.1': 'b4db39db', 'v1.9': 'fae9bd21'}
+this_version = 'v1.9'
