1621fe683cd50b81f1c0378d8e4484f44d28602b
Kataiser <mecharon1.gm@gmail.com>
2019-08-12 14:29:35 -0500

    Added a title to the command window

--- TF2 Rich Presence/Launch Rich Presence alongside TF2.bat
+++ TF2 Rich Presence/Launch Rich Presence alongside TF2.bat
@@ -1,9 +1,9 @@
 @echo off
-"%~dp0\resources\python\python.exe" -OO "%~dp0\resources\welcomer.py" --v 2
+TITLE TF2 Rich Presence {tf2rpvnum}
 
+"%~dp0\resources\python\python.exe" -OO "%~dp0\resources\welcomer.py" --v 2
 "%~dp0\resources\python\python.exe" -OO "%~dp0\resources\launcher.py" --m updater
 
 :start
 "%~dp0\resources\python\python.exe" -OO "%~dp0\resources\launcher.py" --m main
-
 goto start
--- TF2 Rich Presence/Launch TF2 with Rich Presence.bat
+++ TF2 Rich Presence/Launch TF2 with Rich Presence.bat
@@ -1,10 +1,10 @@
 @echo off
-"%~dp0\resources\python\python.exe" -OO "%~dp0\resources\welcomer.py" --v 1
+TITLE TF2 Rich Presence {tf2rpvnum}
 
+"%~dp0\resources\python\python.exe" -OO "%~dp0\resources\welcomer.py" --v 1
 start steam://rungameid/440
 "%~dp0\resources\python\python.exe" -OO "%~dp0\resources\launcher.py" --m updater
 
 :start
 "%~dp0\resources\python\python.exe" -OO "%~dp0\resources\launcher.py" --m main
-
 goto start
