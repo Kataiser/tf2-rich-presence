ffc71e0f982c4486afc116482a28088a62f6d192
Kataiser <mecharon1.gm@gmail.com>
2020-02-17 01:25:39 -0600

    Put the full copyright info in the launcher

--- TF2 Rich Presence/launcher.py
+++ TF2 Rich Presence/launcher.py
@@ -1,5 +1,23 @@
+"""Launcher for TF2 Rich Presence"""
+
+# TF2 Rich Presence
+# https://github.com/Kataiser/tf2-rich-presence
+#
 # Copyright (C) 2019 Kataiser & https://github.com/Kataiser/tf2-rich-presence/contributors
 # https://github.com/Kataiser/tf2-rich-presence/blob/master/LICENSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 import importlib
