9adf98ce8cdeba08e629688049ea9702b549fc67
Kataiser <mecharon1.gm@gmail.com>
2020-03-25 12:14:21 -0500

    Comment stuff in custom.py

--- TF2 Rich Presence/custom.py
+++ TF2 Rich Presence/custom.py
@@ -1,8 +1,9 @@
 # Copyright (C) 2019 Kataiser & https://github.com/Kataiser/tf2-rich-presence/contributors
 # https://github.com/Kataiser/tf2-rich-presence/blob/master/LICENSE
 
-# Add some custom functionality to TF2 Rich Presence if you'd like.
-# Quite limited but someone may find a use it (I know I have).
+# Add some custom functionality to TF2 Rich Presence if you'd like
+# Quite limited but someone may find a use it (I know I have)
+# Also you can replace a .pyd file here with a .py of the same name (sans .cp37-win32) and it'll import
 
 import main
 import logger
@@ -13,16 +14,16 @@ class TF2RPCustom:
         pass
 
     def before_loop(self, app: main.TF2RichPresense):
-        # app.log.debug("Performing tf2rp_custom.before_loop()")
+        # app.log.debug("Performing custom.before_loop()")
         pass
 
     # after TF2RichPresense.activity is finalized but before it's displayed or sent to Discord
     def loop_middle(self, app: main.TF2RichPresense):
-        # app.log.debug("Performing tf2rp_custom.loop_middle()")
+        # app.log.debug("Performing custom.loop_middle()")
         pass
 
     def after_loop(self, app: main.TF2RichPresense):
-        # app.log.debug("Performing tf2rp_custom.after_loop()")
+        # app.log.debug("Performing custom.after_loop()")
         pass
 
 
