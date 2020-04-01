7fce1757b5441f3c02315b2144b31187e4bba886
Kataiser <mecharon1.gm@gmail.com>
2020-04-01 00:57:47 -0500

    Announce a few holidays at startup
    Mainly for the birthday bit but I figured why not a couple more

--- TF2 Rich Presence/init.py
+++ TF2 Rich Presence/init.py
@@ -2,7 +2,11 @@
 # https://github.com/Kataiser/tf2-rich-presence/blob/master/LICENSE
 # cython: language_level=3
 
+import datetime
 import traceback
+from typing import Union
+
+import colorama
 
 import detect_system_language
 import launcher
@@ -22,6 +26,7 @@ def launch(welcome_version):
         welcomer.welcome(log_init, welcome_version)
         detect_system_language.detect(log_init)
         updater.check_for_update(log_init, launcher.VERSION, float(settings.get('request_timeout')))
+        holidays(log_init)
     except (KeyboardInterrupt, SystemExit):
         raise SystemExit
     except Exception:
@@ -31,6 +36,25 @@ def launch(welcome_version):
             pass
 
         raise
+
+
+# cause why not
+def holidays(log: logger.Log):
+    now: datetime.datetime = datetime.datetime.now()
+    holiday_text: Union[str, None] = None
+
+    if now.month == 1 and now.day == 1:
+        holiday_text = "Happy New Years!"
+    elif now.month == 4 and now.day == 1:
+        age = now.year - 2018
+        ordinal = 'tsnrhtdd'[age % 5 * (age % 100 ^ 15 > 4 > age % 10)::4]  # oh
+        holiday_text = f"It's TF2 Rich Presence's {age}{ordinal} birthday today! (Yes, April 1st, seriously)"
+    elif now.month == 12 and now.day == 25:
+        holiday_text = "Merry Christmas!"
+
+    if holiday_text:
+        log.info(f"Today is {now.year}/{now.month}/{now.day}: so the holiday text is \"{holiday_text}\"")
+        print(f"{colorama.Fore.LIGHTGREEN_EX}{holiday_text}{colorama.Fore.RESET}\n")
 
 
 if __name__ == '__main__':
