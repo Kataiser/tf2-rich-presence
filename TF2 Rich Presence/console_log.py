cd21af16012f4146149f70f0a68d36acbd8c10e0
Kataiser <mecharon1.gm@gmail.com>
2020-07-14 16:23:07 -0500

    Added a menus message

--- TF2 Rich Presence/console_log.py
+++ TF2 Rich Presence/console_log.py
@@ -25,7 +25,7 @@ def interpret(self, console_log_path: str, user_usernames: list, kb_limit: float
 
     match_types: Dict[str, str] = {'12v12 Casual Match': 'Casual', 'MvM Practice': 'MvM (Boot Camp)', 'MvM MannUp': 'MvM (Mann Up)', '6v6 Ladder Match': 'Competitive'}
     menus_messages: tuple = ('Server shutting down', 'For FCVAR_REPLICATED', '[TF Workshop]', 'Lobby destroyed', 'Disconnect:', 'destroyed Lobby', 'destroyed CAsyncWavDataCache',
-                             'Disconnecting from abandoned', 'Missing map', 'Host_Error', 'SoundEmitter:')
+                             'Disconnecting from abandoned', 'Missing map', 'Host_Error', 'Kicked due to inactivity', 'SoundEmitter:')
     menus_message: str
     tf2_classes: tuple = ('Scout', 'Soldier', 'Pyro', 'Demoman', 'Heavy', 'Engineer', 'Medic', 'Sniper', 'Spy')
 
