6664e82872d6d3b60fa4639f6d1076b6881a1150
Kataiser <mecharon1.gm@gmail.com>
2020-08-31 14:45:37 -0500

    Comment update

--- TF2 Rich Presence/Change settings.bat
+++ TF2 Rich Presence/Change settings.bat
@@ -1,8 +1,7 @@
 :: Copyright (C) 2019 Kataiser & https://github.com/Kataiser/tf2-rich-presence/contributors
 :: https://github.com/Kataiser/tf2-rich-presence/blob/master/LICENSE
 
-:: Note that these batch launchers don't work from the resources folder (intentionally) and must be moved
-:: one folder up. They are for debugging and function identically to their corresponding EXEs (unless modified).
+:: These batch launchers are for debugging and function identically to their corresponding EXEs (unless modified).
 
 @echo off
 
