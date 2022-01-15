924a66b0fa7032c30a8589e036699032293fdaad
Kataiser <mecharon1.gm@gmail.com>
2022-01-15 00:05:42 -0600

    Removed an outdated comment

--- TF2 Rich Presence/TF2 Rich Presence.bat
+++ TF2 Rich Presence/TF2 Rich Presence.bat
@@ -1,8 +1,6 @@
 :: Copyright (C) 2018-2021 Kataiser & https://github.com/Kataiser/tf2-rich-presence/contributors
 :: https://github.com/Kataiser/tf2-rich-presence/blob/master/LICENSE
 
-:: This batch launcher is for debugging and functions identically to the corresponding EXE (unless modified).
-
 @echo off
 TITLE TF2 Rich Presence ({tf2rpvnum})
 cd "%~dp0"
