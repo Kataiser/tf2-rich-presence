c841bebdf66a27dd18d74a0fee4084c7dac543d5
Kataiser <mecharon1.gm@gmail.com>
2022-10-06 18:24:41 -0500

    Added support for the new Scream Fortress maps

--- TF2 Rich Presence/generate_map_pics.py
+++ TF2 Rich Presence/generate_map_pics.py
@@ -17,9 +17,9 @@ def main():
     # downloads a screenshot of every map and then crops and resizes it for Discord and the GUI
     # maybe don't read this code if you don't need to
 
-    map_datas = [('background01', '/wiki/Background01'), ('devtest', '/wiki/Devtest')]
-    excluded = ('cp_granary', 'arena_nucleus', 'arena_sawmill', 'arena_badlands', 'koth_badlands', 'tr_dustbowl', 'ctf_thundermountain', 'ctf_well', 'arena_well', 'pd_farmageddon',
-                'koth_synthetic_event', 'koth_los_muertos', 'cp_ambush_event', 'pl_terror_event', 'arena_lumberyard_event')
+    map_datas = [('background01', '/wiki/Background01'), ('devtest', '/wiki/Devtest'), ('koth_sawmill_event', '/wiki/Soul-Mill'), ('ctf_helltrain_event', '/wiki/Helltrain'),
+                 ('plr_hacksaw_event', '/wiki/Bonesaw_(map)'), ('ctf_crasher', '/wiki/Crasher'), ('pl_sludgepit_event', '/wiki/Ghoulpit'), ('cp_spookeyridge', '/wiki/Spookeyridge')]
+    excluded = ('cp_granary', 'arena_nucleus', 'arena_sawmill', 'arena_badlands', 'koth_badlands', 'tr_dustbowl', 'ctf_thundermountain', 'ctf_well', 'arena_well')
     overrides = {'mvm_coaltown': '/wiki/File:Coal_Town_base.png', 'mvm_decoy': '/wiki/File:Decoy_left_lane.png', 'mvm_mannworks': '/wiki/File:Mannworks_left_lane.jpg'}
 
     list_page_r = requests.get('https://wiki.teamfortress.com/wiki/List_of_maps')
